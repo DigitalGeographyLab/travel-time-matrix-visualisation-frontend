ac3712cf7297ecb2f14bec71ffa746e45893b033
eemilhaa <eemil.haapanen@gmail.com>
2023-07-03 13:01:10 +0300

    less console.log

--- src/servives/matrices.ts
+++ src/servives/matrices.ts
@@ -2,16 +2,12 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:8080'  // should be handled by proxy
 
 const getMatrix = async (name: string) => {
-  console.log('fetching matrix')
   const response = await axios.get(`${BASE_URL}/${name}.geojson`)
-  console.log('fetched matrix:', response.data)
   return response.data
 }
 
 const getBaseGrid = async () => {
-  console.log('fetching base grid')
   const response = await axios.get(`${BASE_URL}/grid.geojson`)
-  console.log('fetched base grid:', response.data)
   return response.data
 }
 

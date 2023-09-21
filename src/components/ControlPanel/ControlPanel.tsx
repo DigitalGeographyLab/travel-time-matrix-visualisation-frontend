7dc907c92e805898bbb17c13ad748ea646fb9df5
eemilhaa <eemil.haapanen@gmail.com>
2023-09-21 12:16:40 +0300

    include more 2023 variations, hide year select for now

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -3,14 +3,20 @@ import "./style.css"
 
 const ControlPanel = ({setTravelMode, setYear}: any) => {
 
+  const showYearSelect = false
+
   const travelModeOptions = [
-    ['walk_avg', 'walk'],
+    ['walk_avg', 'walk, average'],
+    ['walk_slo', 'walk, slow'],
+    ['bike_avg', 'bike, average'],
     ['bike_slo', 'bike, slow'],
     ['bike_fst', 'bike, fast'],
-    ['pt_r_walk_avg', 'public transport, rush'],
+    ['pt_r_walk_avg', 'public transport, rush hour'],
     ['pt_m_walk_avg', 'public transport, midday'],
+    ['pt_n_walk_avg', 'public transport, night'],
     ['car_r', 'car, rush'],
     ['car_m', 'car, midday'],
+    ['car_n', 'car, night'],
   ]
   const yearOptions = [
     ['2023', '2023'],
@@ -24,12 +30,12 @@ const ControlPanel = ({setTravelMode, setYear}: any) => {
         initialSelection={'pt_r_walk_avg'}
         label={"Travel mode: "}
       />
-      <DropDown
+      {showYearSelect && <DropDown
         setState={setYear}
         options={yearOptions}
         initiallyActive={'2023'}
         label={"Year: "}
-      />
+      />}
     </div>
   )
 }

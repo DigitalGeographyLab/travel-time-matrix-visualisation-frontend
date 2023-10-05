7901f89ef0d4c380649a2c62f40017d7e129ad9b
eemilhaa <eemil.haapanen@gmail.com>
2023-10-05 17:11:10 +0300

    tooltip & legend: make time ranges more clear

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -4,9 +4,9 @@ import "./style.css"
 const Legend = () => {
   const items = [
     ["#FFE841", "0-15"],
-    ["#A69D76", "15-30"],
-    ["#555C6C", "30-45"],
-    ["#001A4B", "45-60"],
+    ["#A69D76", "16-30"],
+    ["#555C6C", "31-45"],
+    ["#001A4B", "46-60"],
   ]
   return (
     <div className="legend">
